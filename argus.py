78b8033d4bdc8fb16c6280255c1b07da4a595f91
Aymen Gannouni <ahmednagnouni@gmail.com>
2018-09-04 16:14:57 +0200

    first commit

--- argus.py
+++ argus.py
@@ -1,12 +1,36 @@
 import click
+import requests as re
+from bs4 import BeautifulSoup
+from tqdm import tqdm
 
 @click.group()
 def cli():
     pass
 
 @cli.command()
-@click.option('--urls', default=[])
+@click.option('--url', default=[])
 @click.option('--days', default=1)
-def populate(urls, days):
-    click.echo(days)
-    click.echo(urls)
+def populate(url, days):
+    ids=getTestIDs(url,days)
+    for id_ in ids:
+        print('\nid:{}\n'.format(id_))
+        print(getData(id_))
+
+def getTestIDs(url,days):
+    page = re.get('https://www.webpagetest.org/testlog.php?days='+str(days)+'&filter='+url+'&all=on')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    tds = soup.find_all('td',{'class': 'url'})
+    return [td.find('a')['href'][8:-1] for td in tds]
+
+def getData(test_id):
+    out={}
+    data = re.get('https://www.webpagetest.org/jsonResult.php?test='+test_id).json()
+    out['TTFB_mean']=data['data']['average']['firstView']['TTFB']
+    out['TTFB_median']=data['data']['average']['firstView']['TTFB']
+    out['connectivity']=data['data']['connectivity']
+    out['id']=data['data']['id']
+    out['from']=data['data']['from']
+    out['location']=data['data']['location']
+    out['mobile']=data['data']['mobile']
+    out['completed']=data['data']['completed']
+    return out
--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ setup(
     py_modules=['argus'],
     install_requires=[
         'Click==6.7',
-        'requests==2.19.1'
+        'requests==2.19.1',
+		'beautifulsoup4==4.6.3',
+		'tqdm==4.25.0'
     ],
     entry_points='''
         [console_scripts]
