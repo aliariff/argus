eb9b2118d3c5b3f1e9d89fb9c29cc7a1c374a4b4
Ali Ariff <ali.ariff12@gmail.com>
2018-09-05 10:44:42 +0200

    update

--- argus.py
+++ argus.py
@@ -7,7 +7,7 @@ from influxdb import InfluxDBClient
 client = InfluxDBClient('localhost', 8086, 'root', 'root', 'example')
 client.create_database('example')
 result = client.query('select value from ttfb;')
-print result
+print(result)
 
 
 @click.group()
--- setup.py
+++ setup.py
@@ -4,12 +4,6 @@ setup(
     name='argus',
     version='0.1',
     py_modules=['argus'],
-    install_requires=[
-        'Click==6.7',
-        'requests==2.19.1',
-		'beautifulsoup4==4.6.3',
-		'influxdb==5.2.0'
-    ],
     entry_points='''
         [console_scripts]
         argus=argus:cli
