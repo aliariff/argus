2b3de50f97d84500bec55ccc9db101ee098dc090
Aymen Gannouni <ahmednagnouni@gmail.com>
2018-09-06 16:26:02 +0200

    added nolimit to url request

--- argus/fetchers/webpagetest.py
+++ argus/fetchers/webpagetest.py
@@ -5,7 +5,7 @@ import requests
 
 def get_test_ids(url, days):
     page = requests.get('https://www.webpagetest.org/testlog.php?days=' +
-                        str(days)+'&filter='+url+'&all=on')
+                        str(days)+'&filter='+url+'&all=on&nolimit=on')
     soup = BeautifulSoup(page.content, 'html.parser')
     tds = soup.find_all('td', {'class': 'url'})
     return [td.find('a')['href'][8:-1] for td in tds]
