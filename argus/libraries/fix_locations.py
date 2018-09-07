5f1aaa3cb2d3ff3881d5d24e3f7c46920dac8e2e
Ali Ariff <ali.ariff12@gmail.com>
2018-09-07 17:01:13 +0200

    fix location loader

--- argus/libraries/fix_locations.py
+++ argus/libraries/fix_locations.py
@@ -1,41 +1,69 @@
 import json
+import os
+from pathlib import Path
 from geotext import GeoText
 
 
 class FixLocations:
-    with open('getLocations.json') as f:
-        locations = json.load(f)
-
     def __init__(self):
-        self.original = locations
+        p = Path(__file__).parents[1]
+        location_file = os.path.join(p, "resources", "get_locations.json")
+        with open(location_file) as f:
+            self.original = json.load(f)
         self.fixed = self.get_fixed_locations()
 
     def get_fixed_locations(self):
-        mix = set(self.original[l]['labelShort'] for l in self.original)
+        mix = set(self.original[l]["labelShort"] for l in self.original)
         countries = set(GeoText(m).countries[0] for m in mix if GeoText(m).countries)
         cities = set(GeoText(m).cities[0] for m in mix if GeoText(m).cities)
-        us_cities = ['California','Chicago','Clifton','Dallas','Denver','Dulles','Lincoln','Los Angeles','New York','Oregon','Orlando','Phoenix','San Francisco','Virginia']
-        missing_cities=set(m.split(',')[0] for m in mix if m.split(',')[0] not in cities)-set(countries)
-        fix={}
+        us_cities = [
+            "California",
+            "Chicago",
+            "Clifton",
+            "Dallas",
+            "Denver",
+            "Dulles",
+            "Lincoln",
+            "Los Angeles",
+            "New York",
+            "Oregon",
+            "Orlando",
+            "Phoenix",
+            "San Francisco",
+            "Virginia",
+        ]
+        missing_cities = set(
+            m.split(",")[0] for m in mix if m.split(",")[0] not in cities
+        ) - set(countries)
+        fix = {}
         for m in mix:
-            fix[m] = {'city': None, 'country': None}
+            fix[m] = {"city": None, "country": None}
             d = GeoText(m)
             if d.cities:
-                fix[m]['city'] = d.cities[0]
+                fix[m]["city"] = d.cities[0]
             if d.countries:
-                fix[m]['country'] = d.countries[0]
+                fix[m]["country"] = d.countries[0]
         for f in fix:
             for c in missing_cities:
-                if c in f: fix[f]['city']=c
-            if 'UK' in f:fix[f]['country']='UK'
-            if fix[f]['city'] == 'Amsterdam': fix[f]['country'] = 'Netherlands'
-            if fix[f]['city'] == 'Brussels': fix[f]['country'] = 'Belgium'
-            if fix[f]['city'] == 'Dubai': fix[f]['country'] = 'UAE'
-            if fix[f]['city'] == 'Seoul': fix[f]['country'] = 'South Korea'
-            if fix[f]['city'] == 'Virginia USA': fix[f]['city'] = 'Virginia'
-            if fix[f]['country'] == 'Singapore': fix[f]['city'] = 'Singapore'
-            if fix[f]['country'] == 'Hong Kong':
-                fix[f]['city'] = 'Hong Kong'
-                fix[f]['country'] = 'China'
-            if fix[f]['city'] in us_cities: fix[f]['country'] = 'USA'
+                if c in f:
+                    fix[f]["city"] = c
+            if "UK" in f:
+                fix[f]["country"] = "UK"
+            if fix[f]["city"] == "Amsterdam":
+                fix[f]["country"] = "Netherlands"
+            if fix[f]["city"] == "Brussels":
+                fix[f]["country"] = "Belgium"
+            if fix[f]["city"] == "Dubai":
+                fix[f]["country"] = "UAE"
+            if fix[f]["city"] == "Seoul":
+                fix[f]["country"] = "South Korea"
+            if fix[f]["city"] == "Virginia USA":
+                fix[f]["city"] = "Virginia"
+            if fix[f]["country"] == "Singapore":
+                fix[f]["city"] = "Singapore"
+            if fix[f]["country"] == "Hong Kong":
+                fix[f]["city"] = "Hong Kong"
+                fix[f]["country"] = "China"
+            if fix[f]["city"] in us_cities:
+                fix[f]["country"] = "USA"
         return fix
--- argus/libraries/locations_details.py
+++ argus/libraries/locations_details.py
@@ -1,22 +1,20 @@
 class LocationDetails:
-
     def __init__(self, locations, location):
         self.locations = locations
-        self.location = self.locations.original[location]['labelShort']
+        self.location = self.locations.original[location]["labelShort"]
         self.meta = self.locations.original[location]
 
     def device(self):
-        if 'Android' in self.meta['group'] or 'Apple' in self.meta['group']:
-            return self.meta['Label']
+        if "Android" in self.meta["group"] or "Apple" in self.meta["group"]:
+            return self.meta["Label"]
         else:
-            return 'Machine'
+            return "Machine"
 
     def city(self):
-        return self.meta['group']
+        return self.meta["group"]
 
     def country(self):
-        return self.locations.fixed[self.location]['country']
+        return self.locations.fixed[self.location]["country"]
 
     def region(self):
-        return self.locations.fixed[self.location]['city']
-
+        return self.locations.fixed[self.location]["city"]
