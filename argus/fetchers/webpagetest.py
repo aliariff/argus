3629b75a697decb5a3cf34f9d505d9993cf59182
Ali Ariff <ali.ariff12@gmail.com>
2018-09-07 10:28:44 +0200

    update

--- argus/fetchers/webpagetest.py
+++ argus/fetchers/webpagetest.py
@@ -1,16 +1,21 @@
+from argus.libraries.url_validator import UrlValidator
 import aiohttp
 import coloredlogs
 import logging
 import requests
-from argus.libraries.url_validator import UrlValidator
+import urllib
 
 logger = logging.getLogger(__name__)
 coloredlogs.install()
 
 
 def get_test_ids(url, days):
+    filter_url = url
+    parsed_uri = urllib.parse.urlparse(url)
+    if parsed_uri.scheme != '':
+        filter_url = parsed_uri.netloc
     page = requests.get('https://www.webpagetest.org/testlog.php?days=' +
-                        str(days)+'&filter='+url+'&all=on&nolimit=on')
+                        str(days)+'&filter='+filter_url+'&all=on&nolimit=on')
     return UrlValidator(url, page.content).get_test_ids()
 
 
